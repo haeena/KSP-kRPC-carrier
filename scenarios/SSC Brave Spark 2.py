c196997d23b0e8831ab6dd8e85e06d0257df2d53
Toshiaki Hatano <haeena@haeena.net>
2017-12-02 19:35:22 +0900

    second vessel lauched with much more science instruments!
    It escaped from atomosphere, separated ascent stage in vacum, then re-entry, succesfully landed.

---
+++ scenarios/SSC Brave Spark 2.py
@@ -0,0 +1,50 @@
+import time
+import krpc
+conn = krpc.connect(name='Sub-orbital flight')
+
+vessel = conn.space_center.active_vessel
+obt_frame = vessel.orbit.body.non_rotating_reference_frame
+srf_frame = vessel.orbit.body.reference_frame
+
+vessel.auto_pilot.target_pitch_and_heading(90, 90)
+vessel.auto_pilot.engage()
+vessel.control.throttle = 1
+time.sleep(1)
+
+print('Launch!')
+vessel.control.activate_next_stage()
+
+fuel_amount = conn.get_call(vessel.resources.amount, 'SolidFuel')
+expr = conn.krpc.Expression.less_than(
+    conn.krpc.Expression.call(fuel_amount),
+    conn.krpc.Expression.constant_float(0.1))
+event = conn.krpc.add_event(expr)
+with event.condition:
+    event.wait()
+
+srf_vertical_speed = conn.get_call(getattr, vessel.flight(srf_frame), 'vertical_speed')
+expr = conn.krpc.Expression.less_than(
+    conn.krpc.Expression.call(srf_vertical_speed),
+    conn.krpc.Expression.constant_double(0))
+event = conn.krpc.add_event(expr)
+with event.condition:
+    event.wait()
+
+print('1St Stage Sep!')
+vessel.control.activate_next_stage()
+
+srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
+expr = conn.krpc.Expression.less_than(
+    conn.krpc.Expression.call(srf_altitude),
+    conn.krpc.Expression.constant_double(2000))
+event = conn.krpc.add_event(expr)
+with event.condition:
+    event.wait()
+
+print('deploy Chute!')
+vessel.control.activate_next_stage()
+
+while vessel.flight(vessel.orbit.body.reference_frame).vertical_speed < -0.1:
+    print('Altitude = %.1f meters' % vessel.flight().surface_altitude)
+    time.sleep(1)
+print('Landed!')
