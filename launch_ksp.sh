ef0730e44f32ded3e2ee844f31aa7f1ea3336c22
Toshiaki Hatano <haeena@haeena.net>
2017-12-30 21:50:04 +0900

    oh my Apple

--- launch_ksp.sh
+++ launch_ksp.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-open ~/Library/Application\ Support/Steam/steamapps/common/Kerbal\ Space\ Program/KSP.app
+
+##
+# macOS High Sieera Glitched Gracphics, so force KSP to use OpenGL
+# See: https://forum.kerbalspaceprogram.com/index.php?/topic/166190-macos-users-please-read/
+~/Library/Application\ Support/Steam/steamapps/common/Kerbal\ Space\ Program/KSP.app/Contents/MacOS/KSP -force-opengl
+
