17819e6ede15a34a671a780d3d6b7b6d25f1f823
Toshiaki Hatano <haeena@haeena.net>
2017-12-30 19:38:46 +0900

    separate poliastro related codes

--- scripts/utils/hohmann_transfer.py
+++ scripts/utils/hohmann_transfer.py
@@ -9,13 +9,12 @@ Body = NewType("Body", object)
 from krpc.client import Client
 from scripts.utils.status_dialog import StatusDialog
 from scripts.utils.execute_node import execute_next_node
+from scripts.utils.krpc_poliastro import krpc_poliastro_bodies
 
 import numpy as np
 
 from astropy import units as AstropyUnit
 from astropy.time import Time as AstropyTime
-from astropy.constants import Constant as AstropyConstant
-from poliastro.bodies import Body as PoliastroBody
 from poliastro.twobody import Orbit as PoliastroOrbit
 from poliastro.maneuver import Maneuver
 
@@ -31,28 +30,6 @@ def angle_between(v1, v2):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-
-def _convert_body_krpc_to_poliastro(poliastro_bodies: dict, parent: PoliastroBody, krpc_body: Body):
-    name = krpc_body.name
-    GM = AstropyConstant('GM_k{}'.format(name), 'Kerbal {} gravitational constant'.format(name),
-                  krpc_body.gravitational_parameter, 'm3 / (s2)', 0,
-                  'kRPC space_center.bodies["{}"].gravitational_parameter'.format(name), system='si')
-    R = AstropyConstant('R_k{}'.format(name), 'Kerbal {} equatorial radius'.format(name),
-                      krpc_body.equatorial_radius, 'm', 0,
-                     'kRPC space_center.bodies["{}"].equatorial_radius'.format(name), system='si')
-    poliastro_body = PoliastroBody(parent, GM, "", name, R)
-    poliastro_bodies[name] = poliastro_body
-    for satelite in krpc_body.satellites:
-        _convert_body_krpc_to_poliastro(poliastro_bodies, poliastro_body, satelite)
-    return
-
-def krpc_poliastro_bodies(conn: Client) -> (dict, dict):
-    poliastro_bodies = {}
-    krpc_bodies = conn.space_center.bodies
-
-    krpc_Sun = krpc_bodies["Sun"]
-    _convert_body_krpc_to_poliastro(poliastro_bodies, None, krpc_Sun)
-    return (krpc_bodies, poliastro_bodies)
 
 def hohmann_transfer_to_target_at_ut(poliastro_bodies: dict, vessel: Vessel, target: Union[Vessel, Body],
                                      ct: float, ut: float, trans_time: float = 0
@@ -124,7 +101,7 @@ def hohmann_transfer_to_target(conn: Client) -> None:
     """
     vessel = conn.space_center.active_vessel
 
-    krpc_bodies, poliastro_bodies = krpc_poliastro_bodies(conn)
+    krpc_bodies, poliastro_bodies = krpc_poliastro_bodies()
 
     # setup stream
     ut = conn.add_stream(getattr, conn.space_center, 'ut')
--- scripts/utils/krpc_poliastro.py
+++ scripts/utils/krpc_poliastro.py
@@ -1,23 +1,22 @@
-from krpc.client import Client
 
-# TODO: type hint for kRPC remote objects may need to be separated
 from typing import Union, NewType
-
 Vessel = NewType("Vessel", object)
 Body = NewType("Body", object)
 
-from astropy import units as u
-from astropy.constants import Constant
+from astropy import units as AstropyUnit
+from astropy.constants import Constant as AstropyConstant
 from poliastro.bodies import Body as PoliastroBody
-from poliastro.twobody import Orbit as PoliastroOrbit
 from poliastro.maneuver import Maneuver
+
+KRPC_BODIES
+POLIASTRO_BODIES
 
 def _convert_body_krpc_to_poliastro(poliastro_bodies: dict, parent: PoliastroBody, krpc_body: Body):
     name = krpc_body.name
-    GM = Constant('GM_k{}'.format(name), 'Kerbal {} gravitational constant'.format(name),
+    GM = AstropyConstant('GM_k{}'.format(name), 'Kerbal {} gravitational constant'.format(name),
                   krpc_body.gravitational_parameter, 'm3 / (s2)', 0,
                   'kRPC space_center.bodies["{}"].gravitational_parameter'.format(name), system='si')
-    R = Constant('R_k{}'.format(name), 'Kerbal {} equatorial radius'.format(name),
+    R = AstropyConstant('R_k{}'.format(name), 'Kerbal {} equatorial radius'.format(name),
                       krpc_body.equatorial_radius, 'm', 0,
                      'kRPC space_center.bodies["{}"].equatorial_radius'.format(name), system='si')
     poliastro_body = PoliastroBody(parent, GM, "", name, R)
@@ -26,13 +25,18 @@ def _convert_body_krpc_to_poliastro(poliastro_bodies: dict, parent: PoliastroBod
         _convert_body_krpc_to_poliastro(poliastro_bodies, poliastro_body, satelite)
     return
 
-def krpc_poliastro_bodies(conn: Client) -> (dict, dict):
-    poliastro_bodies = {}
-    krpc_bodies = conn.space_center.bodies
+def krpc_poliastro_bodies() -> (dict, dict):
+    global KRPC_BODIES, POLIASTRO_BODIES
+    if not POLIASTRO_BODIES:
+        import krpc
+        conn = krpc.connect(name='convert krpc bodies to poliastro bodies')
 
-    krpc_Sun = krpc_bodies["Sun"]
-    _convert_body_krpc_to_poliastro(poliastro_bodies, None, krpc_Sun)
-    return (krpc_bodies, poliastro_bodies)
+        poliastro_bodies = {}
+        krpc_bodies = conn.space_center.bodies
 
-
+        krpc_Sun = krpc_bodies["Sun"]
+        _convert_body_krpc_to_poliastro(poliastro_bodies, None, krpc_Sun)
+        KRPC_BODIES = krpc_bodies
+        POLIASTRO_BODIES = poliastro_bodies
+    return (KRPC_BODIES, POLIASTRO_BODIES)
 
