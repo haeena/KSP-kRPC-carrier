2ae580fdb2b0fb56d59c5f15f45694f783e2d096
Toshiaki Hatano <haeena@haeena.net>
2018-01-01 23:43:00 +0900

    launch ksp in singleton mode

--- launch_ksp.sh
+++ launch_ksp.sh
@@ -3,5 +3,5 @@
 ##
 # macOS High Sieera Glitched Gracphics, so force KSP to use OpenGL
 # See: https://forum.kerbalspaceprogram.com/index.php?/topic/166190-macos-users-please-read/
-~/Library/Application\ Support/Steam/steamapps/common/Kerbal\ Space\ Program/KSP.app/Contents/MacOS/KSP -force-opengl &
+~/Library/Application\ Support/Steam/steamapps/common/Kerbal\ Space\ Program/KSP.app/Contents/MacOS/KSP -single-instance -force-opengl &
 
