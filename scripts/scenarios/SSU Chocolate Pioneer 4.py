ede60dfde6837aa4eb986c12e9229d9dfb6f781a
Toshiaki Hatano <haeena@haeena.net>
2018-01-27 21:35:09 +0900

    launch and back

--- scripts/scenarios/SSU Chocolate Pioneer 4.py
+++ scripts/scenarios/SSU Chocolate Pioneer 4.py
@@ -1,3 +1,4 @@
+import math
 import time
 import krpc
 from scripts.utils.launch_into_orbit import launch_into_orbit
@@ -15,28 +16,61 @@ def main():
     ut = conn.add_stream(getattr, conn.space_center, 'ut')
 
     # launch into low kerbal orbit
-    #launch_into_orbit(conn, TARGET_ALTITUDE, TARGET_INCLINATION, auto_stage=False)
+    launch_into_orbit(conn, TARGET_ALTITUDE, TARGET_INCLINATION, auto_stage=False)
 
     # circulize again
-    #next_time_ap = ut() + vessel.orbit.time_to_apoapsis 
-    #circularize(conn, next_time_ap)
+    next_time_ap = ut() + vessel.orbit.time_to_apoapsis 
+    circularize(conn, next_time_ap)
 
     # our target body is Mun!
-    #conn.space_center.target_body = conn.space_center.bodies["Mun"]
-    #hohmann_transfer_to_target(conn)
+    conn.space_center.target_body = conn.space_center.bodies["Mun"]
+    hohmann_transfer_to_target(conn)
     
-    # warp to soi change
-    #conn.space_center.warp_to(ut() + vessel.orbit.time_to_soi_change)
-    #time.sleep(5)
+    # warp to soi change (Kerbin -> Mun)
+    conn.space_center.warp_to(ut() + vessel.orbit.time_to_soi_change + 30)
+    time.sleep(5)
 
-    # change periapsis altitude of mun to 30km at 5min
+    ## Flyby Mun
     change_periapsis(conn, ut() + 300, 30000)
-    # TODO: ok this does't work since prograde is targetting straight into Mun CoG
 
-    # warp to soi change
-    conn.space_center.warp_to(ut() + vessel.orbit.time_to_soi_change)
+    # warp to soi change (Mun -> Kerbin)
+    conn.space_center.warp_to(ut() + vessel.orbit.time_to_soi_change + 30)
+    time.sleep(5)
 
+    ## reentry
+    change_periapsis(conn, ut() + 300, 40000)
 
+    conn.space_center.warp_to(ut() + vessel.orbit.time_to_periapsis - 120)
+
+    vessel.auto_pilot.engage()
+    vessel.auto_pilot.reference_frame = vessel.surface_velocity_reference_frame
+    vessel.auto_pilot.target_heading = 180
+    time.sleep(5)
+    vessel.auto_pilot.wait()
+
+    vessel.control.throttle = 1.0
+
+    periapsis_altitude = conn.get_call(getattr, vessel.orbit, 'periapsis_altitude')
+    expr = conn.krpc.Expression.less_than(
+        conn.krpc.Expression.call(periapsis_altitude),
+        conn.krpc.Expression.constant_double(0))
+    event = conn.krpc.add_event(expr)
+    with event.condition:
+        event.wait()
+    vessel.control.throttle = 0.0
+    time.sleep(5)
+
+    vessel.control.activate_next_stage()
+
+    srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
+    expr = conn.krpc.Expression.less_than(
+        conn.krpc.Expression.call(srf_altitude),
+        conn.krpc.Expression.constant_double(2000))
+    event = conn.krpc.add_event(expr)
+    with event.condition:
+        event.wait()
+
+    vessel.control.activate_next_stage()
 
 if __name__ == "__main__":
     main()
