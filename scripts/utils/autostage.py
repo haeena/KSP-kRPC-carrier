8a5d6129f0e6c12e33c7d1650b5e0c0f8e03325b
Toshiaki Hatano <haeena@haeena.net>
2018-05-30 21:46:47 +0900

    remove autostage() function no longer used

--- scripts/utils/autostage.py
+++ scripts/utils/autostage.py
@@ -23,6 +23,7 @@ def set_autostaging(conn: Client,
         return nothing, return when procedure finished
 
     """
+    # TODO: global without lock
     global is_autostaging
     is_autostaging = True
 
@@ -84,41 +85,6 @@ def set_autostaging(conn: Client,
 def unset_autostaging():
     global is_autostaging
     is_autostaging = False
-
-def autostage(conn: Client,
-              liquid_fuel: bool = True, oxidizer: bool = True,
-              solid_fuel: bool = True) -> None:
-    """autostage if current stage is empty
-
-    Extended description of function.
-
-    Args:
-        conn: kRPC connection
-
-    Returns:
-        return nothing, return when procedure finished
-
-    """
-    vessel = conn.space_center.active_vessel
-    dialog = StatusDialog(conn)
-
-    resource_types_for_stage = []
-    if liquid_fuel:
-        resource_types_for_stage.append("LiquidFuel")
-    if oxidizer:
-        resource_types_for_stage.append("Oxidizer")
-    if solid_fuel:
-        resource_types_for_stage.append("SolidFuel")
-
-    # Staging when fuel empty
-    current_stage = vessel.control.current_stage
-
-    resources_of_stage = vessel.resources_in_decouple_stage(current_stage - 1)
-    max_resource = max(map(lambda r: resources_of_stage.amount(r), resource_types_for_stage))
-
-    if max_resource <= 0.1:
-        dialog.status_update("Staging: stage {}".format(current_stage - 1))
-        vessel.control.activate_next_stage()
 
 if __name__ == "__main__":
     import os
