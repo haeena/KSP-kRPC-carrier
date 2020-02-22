accd09908a382bbfa53993c854a24cfca4b1abfc
Toshiaki Hatano <haeena@haeena.net>
2020-02-22 14:14:57 +0900

    temporary fix for krpc 1.9 build

--- scripts/utils/status_dialog.py
+++ scripts/utils/status_dialog.py
@@ -1,17 +1,20 @@
 import krpc
 
+
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
 
-class StatusDialog(object, metaclass=Singleton):
 
+class StatusDialog(metaclass=Singleton):
     def __init__(self, conn: krpc.connection = None):
+        return
         if not conn:
-            conn = krpc.connect(name='Status Dialog')
+            conn = krpc.connect(name="Status Dialog")
 
         # setup canvas
         canvas = conn.ui.stock_canvas
@@ -21,23 +24,28 @@ class StatusDialog(object, metaclass=Singleton):
 
         rect = panel.rect_transform
         rect.size = (400, 50)
-        rect.position = (250-(screen_size[0]/2), 400)
+        rect.position = (250 - (screen_size[0] / 2), 400)
 
         self.text_status = panel.add_text("")
         text_status = self.text_status
-        text_status.rect_transform.size = (380,40)
+        text_status.rect_transform.size = (380, 40)
         text_status.rect_transform.position = (0, 0)
         text_status.color = (1, 1, 1)
         text_status.size = 12
 
     def status_update(self, message):
         # TODO: when active vessel switched, dialog window cleared, so re-create panel required
-        status_line = "Status: {}".format(message)
+        status_line = f"Status: {message}"
         print(status_line)
+        return
         self.text_status.content = status_line
+
 
 if __name__ == "__main__":
     import time
+
     sd = StatusDialog()
-    sd.status_update("this is test, how log messages can we go? I don't know but let's do some test")
+    sd.status_update(
+        "this is test, how log messages can we go? I don't know but let's do some test"
+    )
     time.sleep(5)
