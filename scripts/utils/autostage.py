02c53217e68c57e61b013f09b1535219602edd76
Toshiaki Hatano <haeena@haeena.net>
2020-02-22 14:17:03 +0900

    it's not TODO

--- scripts/utils/autostage.py
+++ scripts/utils/autostage.py
@@ -1,18 +1,19 @@
-import time
-import math
-from functools import reduce
 from krpc.client import Client
 from scripts.utils.status_dialog import StatusDialog
+
 
 is_autostaging = True
 staging_event = None
 
-def set_autostaging(conn: Client,
-                    liquid_fuel: bool = True,
-                    oxidizer: bool = True,
-                    solid_fuel: bool = True,
-                    threashold: float = 0,
-                    stop_stage: int = 0) -> None:
+
+def set_autostaging(
+    conn: Client,
+    liquid_fuel: bool = True,
+    oxidizer: bool = True,
+    solid_fuel: bool = True,
+    threashold: float = 0,
+    stop_stage: int = 0,
+) -> None:
     """set next autostagin on resource is under certin level
 
     Extended description of function.
@@ -35,27 +36,46 @@ def set_autostaging(conn: Client,
     if current_stage <= stop_stage:
         return
 
-    resources_in_next_decoupled_stage = vessel.resources_in_decouple_stage(current_stage - 1)
+    resources_in_next_decoupled_stage = vessel.resources_in_decouple_stage(
+        current_stage - 1
+    )
 
     # TODO: offset must take account unused (undrained) resource, not only SF, that could be LF and/or O also?
-    # ...or, completely replace staging logic like: just check all engines in decoupled stage and stage when all active engine doens't have fuel 
+    # ...or, completely replace staging logic like: just check all engines in decoupled stage and stage when all active engine doens't have fuel
     # downside of this logic, is we cannot take fuel threashold other than 0, that make powered recovery impossible
     resource_types_for_stage = []
-    if liquid_fuel and resources_in_next_decoupled_stage.has_resource("LiquidFuel"):
+    if liquid_fuel and resources_in_next_decoupled_stage.has_resource(
+        "LiquidFuel"
+    ):
         resource_types_for_stage.append("LiquidFuel")
     if oxidizer and resources_in_next_decoupled_stage.has_resource("Oxidizer"):
         resource_types_for_stage.append("Oxidizer")
-    if solid_fuel and resources_in_next_decoupled_stage.has_resource("SolidFuel"):
+    if solid_fuel and resources_in_next_decoupled_stage.has_resource(
+        "SolidFuel"
+    ):
         # check solid fuel only if there's active srbs decoupled next
-        srbs_next_decoupled = [e for e in vessel.parts.engines if e.part.resources.has_resource("SolidFuel") and e.part.decouple_stage == (current_stage - 1)]
-        if len([ e for e in srbs_next_decoupled if e.active ]) > 0:
+        srbs_next_decoupled = [
+            e
+            for e in vessel.parts.engines
+            if e.part.resources.has_resource("SolidFuel")
+            and e.part.decouple_stage == (current_stage - 1)
+        ]
+        if len([e for e in srbs_next_decoupled if e.active]) > 0:
             # calculate solid fuel offset, fuels in non active srbs decoupled next (separetron)
-            solidfuel_unused_decoupled_in_next_stage = sum([e.part.resources.amount("SolidFuel") for e in srbs_next_decoupled if not e.active])
+            solidfuel_unused_decoupled_in_next_stage = sum(
+                [
+                    e.part.resources.amount("SolidFuel")
+                    for e in srbs_next_decoupled
+                    if not e.active
+                ]
+            )
             resource_types_for_stage.append("SolidFuel")
     if len(resource_types_for_stage) == 0:
         # if current stage has empty resource (fairing etc.) just stage
         vessel.control.activate_next_stage()
-        set_autostaging(conn, liquid_fuel, oxidizer, solid_fuel, threashold, stop_stage)
+        set_autostaging(
+            conn, liquid_fuel, oxidizer, solid_fuel, threashold, stop_stage
+        )
         return
 
     expression = conn.krpc.Expression
@@ -63,24 +83,25 @@ def set_autostaging(conn: Client,
     call_current_stage = conn.get_call(getattr, vessel.control, "current_stage")
     staging_condition = expression.not_equal(
         expression.call(call_current_stage),
-        expression.constant_int(current_stage)
+        expression.constant_int(current_stage),
     )
 
     for resource in resource_types_for_stage:
         resource_threashold = threashold
         if resource == "SolidFuel":
             resource_threashold += solidfuel_unused_decoupled_in_next_stage
-        resource_amount_call = conn.get_call(resources_in_next_decoupled_stage.amount, resource)
+        resource_amount_call = conn.get_call(
+            resources_in_next_decoupled_stage.amount, resource
+        )
         cond = expression.less_than_or_equal(
-                conn.krpc.Expression.call(resource_amount_call),
-                conn.krpc.Expression.constant_float(resource_threashold)
-            )
+            conn.krpc.Expression.call(resource_amount_call),
+            conn.krpc.Expression.constant_float(resource_threashold),
+        )
         staging_condition = expression.or_(staging_condition, cond)
 
     global staging_event
     staging_event = conn.krpc.add_event(staging_condition)
 
-    # TODO: global without lock
     def auto_staging():
         global is_autostaging
         remove_staging_events()
@@ -91,10 +112,13 @@ def set_autostaging(conn: Client,
         # check if stage triggered by somewhere else
         if current_stage == vessel.control.current_stage:
             vessel.control.activate_next_stage()
-        set_autostaging(conn, liquid_fuel, oxidizer, solid_fuel, threashold, stop_stage)
+        set_autostaging(
+            conn, liquid_fuel, oxidizer, solid_fuel, threashold, stop_stage
+        )
 
     staging_event.add_callback(auto_staging)
     staging_event.start()
+
 
 def remove_staging_events():
     global staging_event
@@ -102,17 +126,20 @@ def remove_staging_events():
         staging_event.remove()
         staging_event = None
 
+
 # TODO: global without lock
 def unset_autostaging():
     global is_autostaging
     is_autostaging = False
     remove_staging_events()
 
+
 if __name__ == "__main__":
     import os
     import krpc
+
     krpc_address = os.environ["KRPC_ADDRESS"]
-    conn = krpc.connect(name='autostage', address=krpc_address)
+    conn = krpc.connect(name="autostage", address=krpc_address)
     set_autostaging(conn)
 
     print("start sleeping")
